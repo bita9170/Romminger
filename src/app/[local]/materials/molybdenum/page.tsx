7ff19afbe03754267f2d6a15fc50681267f1ccb6
bita9170 <bita.s9170@gmail.com>
2024-03-19 13:05:11 +0100

    removed ul & li from Molybdenums page

--- src/app/[local]/materials/molybdenum/page.tsx
+++ src/app/[local]/materials/molybdenum/page.tsx
@@ -8,15 +8,15 @@ const Molybdenumpage = () => {
     <div className="bio">
       <h1 className="heading">{t("Molybdenum.title")}</h1>
       <p>{t("Molybdenum.text")}<br /> <br /> </p>
-      <ul className="list-disc">
-        <li><strong>{t("Molybdenum.strong1")}</strong> {t("Molybdenum.physicalProperties")}</li><br />
-        <li><strong>{t("Molybdenum.strong2")}</strong> {t("Molybdenum.chemicalProperties")}</li>  <br />
-        <li><strong>{t("Molybdenum.strong3")}</strong> {t("Molybdenum.magneticProperties")}</li>  <br />
-        <li><strong>{t("Molybdenum.strong4")}</strong> {t("Molybdenum.electricalConductivity")}</li>  <br />
-        <li><strong>{t("Molybdenum.strong5")}</strong> {t("Molybdenum.corrosionResistance")}</li>  <br />
-        <li><strong>{t("Molybdenum.strong6")}</strong> {t("Molybdenum.highMeltingTemperature")}</li> <br />
-        <li><strong>{t("Molybdenum.strong7")}</strong> {t("Molybdenum.catalyticProperties")}</li> 
-      </ul>
+      
+      <strong>{t("Molybdenum.strong1")}</strong> {t("Molybdenum.physicalProperties")}<br /> <br />
+      <strong>{t("Molybdenum.strong2")}</strong> {t("Molybdenum.chemicalProperties")}  <br /> <br />
+      <strong>{t("Molybdenum.strong3")}</strong> {t("Molybdenum.magneticProperties")}  <br /> <br />
+      <strong>{t("Molybdenum.strong4")}</strong> {t("Molybdenum.electricalConductivity")}  <br /> <br />
+      <strong>{t("Molybdenum.strong5")}</strong> {t("Molybdenum.corrosionResistance")}  <br /> <br />
+      <strong>{t("Molybdenum.strong6")}</strong> {t("Molybdenum.highMeltingTemperature")} <br /> <br />
+      <strong>{t("Molybdenum.strong7")}</strong> {t("Molybdenum.catalyticProperties")} 
+      
       
     </div>
   );
